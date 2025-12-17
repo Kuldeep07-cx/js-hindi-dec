d0866cecd9ec069fbbffbdd5242880bb352081b0
Kuldeep07-cx <113789008+Kuldeep07-cx@users.noreply.github.com>
2025-12-17 10:14:05 +0000

    changes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+const  accountId = 1213
+let kuldeep = "warrior"
+let accountEmail = "kuldeep78@gmail.com"
+accountplace= "delhi"
+var accountPassword="lol"
+let accountGm = 4646;
+
+
+ console.log(typeof accountId)
+  console.log(typeof accountGm)
+  console.log(typeof accountEmail)
+   console.log(typeof null)
+    console.log(typeof undefined)
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const  accountId = 1213
+let kuldeep = "warrior"
+let accountEmail = "kuldeep78@gmail.com"
+accplace = "delhi"
+var accountPassword="lol"
+
+console.log(accountId)
+console.log(kuldeep)
+console.log(accplace)
+console.log(accountPassword)
+
+
+
+kuldeep = "kiwi"
+accplace = "delhi"
+accountPassword = "Jana bhai"
+accountEmail = "jana@gmail.com"
+
+
+console.table([accountId,kuldeep,accplace,accountPassword])
