8510963f6871f33165761a99ea9018a977575148
Kuldeep07-cx <113789008+Kuldeep07-cx@users.noreply.github.com>
2025-12-18 04:31:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,19 @@
+let score = "25"
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof(valueInNumber));
+
+/*number to string*/
+
+let someNumber = 33
+let stringnumber = String(someNumber)
+console.log(stringnumber)
+console.log(typeof(stringnumber))
+
+// boolean conversion & and it gives true value 
+
+let IsLoggedIn =1
+let booleanIsloggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsloggedIn)
+
