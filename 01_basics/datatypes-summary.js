7b76be61f5a8fe885c3b8a5db42563ba98a20cb3
Kuldeep07-cx <113789008+Kuldeep07-cx@users.noreply.github.com>
2025-12-20 17:39:49 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+// Primitive
+
+// 7 types: String, Number , Bigint,  Boolean , null, undefined, Symbol
+ 
+
+
+
+
+//Reference (Non primitive)
+
+// Array, Object, functions
+
+const heros = ["kuldeep" , "singh" , "rawat"];
+let myObj = {
+    name: "kuldeep",
+    age : 23,
+}
+
