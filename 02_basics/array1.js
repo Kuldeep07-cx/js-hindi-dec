7e241709006e836cba38b7dbaf923cb13cdbf898
Kuldeep07-cx <113789008+Kuldeep07-cx@users.noreply.github.com>
2025-12-23 05:07:19 +0000

    array1

--- 02_basics/array1.js
+++ 02_basics/array1.js
@@ -1,1 +1,16 @@
 
+let arr =[1,2,4,5,]
+console.log(arr[6/2])
+
+let myArray = new Array(1,2,3,4,5,6);
+console.log(myArray[2]);
+
+
+// Array methods
+myArray.push(7)
+myArray.push(12)
+// myArray.pop() // remove from last
+
+myArray.unshift(10); // add in the place of index 0
+myArray.shift()  // removes 0th index element 
+console.log(myArray);
