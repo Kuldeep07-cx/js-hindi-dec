2d569bc82255e9e9ce4aa892b0438918b02a704f
Kuldeep07-cx <113789008+Kuldeep07-cx@users.noreply.github.com>
2025-12-30 18:42:00 +0000

    arrow khtm

---
+++ 03_basics/arrow.js
@@ -0,0 +1,59 @@
+//  This used for the current context
+
+const user = {
+    username: "kuldeep rawat",
+    id: 7065,
+    welcomeMessage: function(){ 
+    console.log(`${this.username}, welcome to this site and your id is ${this.id} `);
+    console.log(this)
+    }
+}
+
+//user.welcomeMessage()
+//user.username ="sunny rawat"
+//user.welcomeMessage()
+
+//console.log(this)
+
+ //function chai(){
+   // let username = "kuldeep"
+   // console.log(this.username);
+//}
+
+//chai()
+
+// const chai= function(){
+// let username = "kuldeep"
+// console.log(this.username);
+// }
+
+ //chai()
+
+ const chai = () => {
+    let username = "kuldeep"
+    console.log(this);
+ }
+
+chai()
+
+
+// Basic arrow function4
+
+
+
+//Implicit 
+//const addtwo = (num1, num2) => num1 + num2;
+const addtwo = (num1, num2) => ({name:"kuldeep"})
+
+console.log(addtwo(2, 3));
+
+
+
+//explicit
+
+const add2 = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(add2(2, 3)); // 5
+
