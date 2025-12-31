2af8a014aee100f65ef2e1dff66c46f94b23e0b0
Kuldeep07-cx <113789008+Kuldeep07-cx@users.noreply.github.com>
2025-12-31 09:25:01 +0000

    while and do while

---
+++ 05_iteration/two.js
@@ -0,0 +1,30 @@
+// let i =0;
+
+// while(i<=20){
+//     console.log(`value of i is ${i}`);
+//     i= i +2;
+// }
+// console.log(`index of i is ${i}`)
+
+
+
+
+// while is a entry controlled loop 
+//do while is a exit controlled loop
+let arr = ["kam","kul","san"]
+
+let k = 0
+while(k<arr.length){
+    console.log(`the value is ${arr[k]}`);
+    k++;
+}
+
+
+
+let score = 30
+do{ 
+    console.log(`the score is ${score} `)
+    score++;
+
+}
+while( score<=30)
